c5ac6e189549cc3268ee8baf5114265f0d367756
Drew Cain <groksrc@gmail.com>
2019-02-28 12:34:50 -0600

    my first serverless service

---
+++ README.md
@@ -0,0 +1,13 @@
+# Serverless
+
+Welcome to my first serverless service.
+
+## Functions
+* hello
+* goodbye
+
+## Deploy
+`$ sls deploy`
+
+## Invoke
+`$ sls invoke -f <function> -l
---
+++ handler.js
@@ -0,0 +1,25 @@
+'use strict';
+
+module.exports.hello = async (event, context) => {
+  return {
+    statusCode: 200,
+    body: JSON.stringify({
+      context,
+      message: 'Go Serverless v1.0! Your function executed successfully!',
+      input: event,
+    }),
+  };
+
+  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
+  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
+};
+
+module.exports.goodbye = async (event, context) => {
+  return {
+    statusCode: 200,
+    body: JSON.stringify({
+      message: 'Go forth and conquer!',
+      input: event
+    })
+  }
+}
